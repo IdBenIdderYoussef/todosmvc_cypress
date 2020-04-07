259cabcada847f71340f15655a54a435ec07cda4
Otmane Essakouri <Abd.Alah@gmail.com>
2020-04-07 14:03:51 +0100

    Delete all items + Delete all completed items

--- cypress/integration/app_todo.spec.js
+++ cypress/integration/app_todo.spec.js
@@ -1,4 +1,8 @@
-import {TODO_ONE, TODO_TWO, TODO_THREE} from './defaultTodos'
+const TODO_ONE = 'to do task 1';
+const TODO_TWO = 'to do task 2';
+const TODO_THREE = 'to do task 3';
+const TODO_FOUR = 'to do task 4';
+const TODO_FIVE = 'to do task 5';
 
 describe('TodoMvc Test', function () {
 
@@ -7,54 +11,49 @@ describe('TodoMvc Test', function () {
     });
 
     it('should add one todo', () => {
-        cy.createTodos([TODO_ONE]);
+        cy.initTodosList([TODO_ONE]);
         cy.shouldExist(TODO_ONE);
-    });
+    })
 
     it('should delete one todo', () => {
 
-        cy.createTodos([TODO_ONE, TODO_TWO]);
+        cy.initTodosList([TODO_ONE, TODO_TWO]).as('todoList');
 
-        cy.get(`input[value="${TODO_ONE}"]`)
-            .parent()
-            .children('.view')
-            .children('.destroy')
-            .invoke('attr', 'style', 'display: block')
-            .click();
+        cy.get('@todoList').contains(TODO_ONE).parent()
+        .find('.destroy')
+        .invoke('show').click()
 
         cy.get('.todo-list').contains(TODO_ONE).should('not.exist');
         cy.shouldExist(TODO_TWO);
-    });
+    })
 
 
-    it('should delete all items', function () {
-        cy.createTodos([TODO_ONE, TODO_TWO, TODO_THREE]);
-        cy.get('.todo-list li').each(() => {
-            cy.get('.destroy').click({force: true, multiple: true})
-        });
-        cy.shouldHaveLength(0);
-    });
+    // Delete All tasks 
+    it('should delete all tasks', ()=>{
+        
+        cy.initTodosList([TODO_ONE, TODO_TWO, TODO_THREE, TODO_FOUR, TODO_FIVE]).as('todoList')
+        
+        cy.get('@todoList').find('.view>label').each(($el)=>{
+            cy.get('@todoList').contains($el.text()).parent().find('.destroy').invoke('show').click()
+        })
+        cy.get('@todoList').should('have.length',0)
+    })
+    
 
-    it('should delete completed items', function () {
-        cy.createTodos([TODO_ONE, TODO_TWO, TODO_THREE]);
-        cy.toggleTodo(TODO_ONE);
-        cy.toggleTodo(TODO_TWO);
-
-        cy.get('.clear-completed')
-            .should('be.visible')
-            .click();
-
-        cy.get('.clear-completed')
-            .should('not.exist');
-
-        cy.shouldHaveLength(1);
-        cy.shouldExist(TODO_THREE);
-
-
-    });
+    // Delete all completed tasks
+    it('should delete completed tasks', ()=>{
+        cy.initTodosList([TODO_ONE, TODO_TWO, TODO_THREE, TODO_FOUR, TODO_FIVE]).as('todoList')
+        cy.checkTasksAsCompleted([TODO_ONE, TODO_TWO, TODO_FOUR])
+        cy.get('.clear-completed').should('be.visible').click()
+        cy.get('.clear-completed').should('not.exist')
+        cy.get('@todoList').should('have.length',2)
+        cy.get('@todoList').should( 'contain' , TODO_THREE )
+        cy.get('@todoList').should( 'contain' , TODO_FIVE )
+        
+    })
 
     it('should display active items', function () {
-        cy.createTodos([TODO_ONE, TODO_TWO, TODO_THREE]);
+        cy.initTodosList([TODO_ONE, TODO_TWO, TODO_THREE]);
         cy.toggleTodo(TODO_TWO);
         cy.displayTodos('Active');
         cy.shouldExist(TODO_ONE);
@@ -64,7 +63,7 @@ describe('TodoMvc Test', function () {
 
 
     it('should display completed items', function () {
-        cy.createTodos([TODO_ONE, TODO_TWO, TODO_THREE]);
+        cy.initTodosList([TODO_ONE, TODO_TWO, TODO_THREE]);
         cy.toggleTodo(TODO_TWO);
         cy.displayTodos('Completed');
         cy.shouldExist(TODO_TWO);
@@ -72,14 +71,14 @@ describe('TodoMvc Test', function () {
     });
 
     it('should display all items', function () {
-        cy.createTodos([TODO_ONE, TODO_TWO, TODO_THREE]);
+        cy.initTodosList([TODO_ONE, TODO_TWO, TODO_THREE]);
         cy.toggleTodo(TODO_TWO);
         cy.displayTodos('All');
         cy.shouldHaveLength(3);
     });
 
     it('should display current filter', function () {
-        cy.createTodos([TODO_ONE, TODO_TWO, TODO_THREE]);
+        cy.initTodosList([TODO_ONE, TODO_TWO, TODO_THREE]);
         cy.shouldFilterSelected('All');
         cy.displayTodos('Active');
         cy.shouldFilterSelected('Active');
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,34 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
-import {TODO_ONE, TODO_TWO, TODO_THREE} from '../integration/defaultTodos';
-
-Cypress.Commands.add('createTodos', function (todos) {
-    todos.map(todo => cy.get('.new-todo').type(`${todo}{enter}`));
+Cypress.Commands.add('initTodosList', function (todos) {
+    todos.map(todo => cy.get('.new-todo').type(todo.concat(' {enter}')));
+    return cy.get('.todo-list li')
 });
+
+// this command check as completed all tasks received in the Array Parameter 'TasksList'
+Cypress.Commands.add("checkTasksAsCompleted",(TasksList)=>{
+    cy.wrap(TasksList).each((Task, index, ListOfTasksList)=>{
+        cy.get('.todo-list li .view>label').contains(Task).parent().find('.toggle').check();
+    })
+})
 
 Cypress.Commands.add('displayTodos', function (category) {
     cy.get('.filters')
